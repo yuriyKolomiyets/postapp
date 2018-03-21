08dbbd8753e7f0a19d23e7c8fbfd4020d06449b5
yuriykolomiyets <juriy121@gmail.com>
2018-03-21 15:34:36 +0200

    model test add

--- src/test/java/model/CustomerTest.java
+++ src/test/java/model/CustomerTest.java
@@ -1,14 +1,20 @@
 package model;
-
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 
 public class CustomerTest {
 
+    Customer testUser = new Customer("test3@gmail.com", "123456", "344", "Igor");
+
+
     @Test
     public void getPhone() {
+        assertEquals("123456", testUser.getPhone());
     }
 
     @Test
     public void getName() {
+        assertEquals("Igor", testUser.getName());
     }
 }
